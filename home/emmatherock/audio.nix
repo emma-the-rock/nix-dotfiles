3c435a8b497e8de9665e688195b5ff6352550848
emmatherock <emanuelxoda@gmail.com>
2026-06-19 23:19:16 -0300

    home manager: add carla service

--- home/emmatherock/audio.nix
+++ home/emmatherock/audio.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   xdg.configFile = {
     "pipewire/pipewire.conf.d/10-discord.conf".source = ./pipewire/10-discord.conf;
@@ -5,5 +6,29 @@
     "pipewire/pipewire.conf.d/10-misc.conf".source = ./pipewire/10-misc.conf;
     "pipewire/pipewire.conf.d/10-music.conf".source = ./pipewire/10-music.conf;
     "pipewire/pipewire.conf.d/10-vban-send.conf".source = ./pipewire/10-vban-send.conf;
+  };
+  systemd.user.services.carla = {
+    Unit = {
+      Description = "Carla Audio Host";
+      After = [
+        "pipewire.service"
+        "pipewire-pulse.service"
+      ];
+    };
+
+    Service = {
+      Type = "simple";
+      Environment = [
+        "DISPLAY=:0"
+        "XDG_RUNTIME_DIR=/run/user/1000"
+      ];
+      ExecStart = "${pkgs.steam-run}/bin/steam-run ${pkgs.carla}/bin/carla --nogui /mnt/ssd/conf/patchbay.carxp";
+      Restart = "always";
+      RestartSec = 3;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 }
